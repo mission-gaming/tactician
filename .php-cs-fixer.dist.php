44cb9301d7bb8533c5eaa5da993bd2b96702b512
Jamie Hollern <jamiehollern@gmail.com>
2025-09-12 02:58:48 +0100

    Prevent PHP CS Fixer from converting double asterisk comments to singles.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -79,7 +79,7 @@ return (new PhpCsFixer\Config())
         'phpdoc_scalar' => true,
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_summary' => true,
-        'phpdoc_to_comment' => true,
+        'phpdoc_to_comment' => false,
         'phpdoc_trim' => true,
         'phpdoc_types' => true,
         'phpdoc_var_without_name' => true,
